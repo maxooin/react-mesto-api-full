0edd0e2867d36ad1039d2d4d2ae779284d76429c
Mikhail Kotin <maxooin@yandex.ru>
2023-02-15 13:39:08 +0300

    upd: Update cors settings

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,6 +1,8 @@
 const allowedCors = [
   'http://localhost:3000',
-  'https://localhost:3000'
+  'https://localhost:3000',
+  'https://maxooin.nomoredomains.work/',
+  'http://maxooin.nomoredomains.work/'
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
