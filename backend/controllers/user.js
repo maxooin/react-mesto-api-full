c7e42a3a310116932e53c78942654eedd5c80028
Mikhail Kotin <maxooin@yandex.ru>
2023-02-15 21:51:11 +0300

    upd: Update api frontend and backend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -107,8 +107,8 @@ export function updateUserInfo(req, res, next) {
 }
 
 export function updateUserAvatar(req, res, next) {
-  const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, {
+  const {avatar} = req.body;
+  User.findByIdAndUpdate(req.user._id, {avatar}, {
     new: true,
     runValidators: true,
   })
@@ -136,16 +136,11 @@ export function login(req, res, next) {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign(
-        { _id: user._id },
+        {_id: user._id},
         NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret',
-        { expiresIn: '7d' },
+        {expiresIn: '7d'},
       );
-      res.cookie('jwt', token, {
-        maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
-        sameSite: true,
-      });
-      res.send({ token });
+      res.send({token});
     })
     .catch(() => {
       next(new UnauthorizedError('Передан неверный логин или пароль'));
